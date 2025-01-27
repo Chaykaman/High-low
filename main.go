90e4187f49da98ba0f21f666c3c9f8891eae7a9d
Chaykaman <chinchopa335@gmail.com>
2025-01-27 04:34:38 +0300

    Update main.go
    Add lifes to guess

--- main.go
+++ main.go
@@ -15,23 +15,34 @@ func check() {
 	var usernumber int
 	generatornumber()
 	var programnumber int = generatornumber()
-	fmt.Println("Угадай какое я загадал число)))")
+	fmt.Println("Угадай какое я загадал число за 3 попытки)))")
 	fmt.Scanf("%d\n", &usernumber)
-	if usernumber > programnumber {
-		fmt.Println("АХАха Лох твоё число больше загаданного. Загаданным число было: ", programnumber)
+	for i := 2; i >= 0; i-- {
+		if usernumber > programnumber {
+			fmt.Println("АХАха Лох твоё число больше загаданного.")
 
-	} else if usernumber < programnumber {
-		fmt.Println("Аахаха Лох твоё число меньше загаданного.Загаданным число было: ", programnumber)
+		} else if usernumber < programnumber {
+			fmt.Println("Аахаха Лох твоё число меньше загаданного.")
 
-	} else {
-		fmt.Println("Хорош!!! Ты угадал!!!! Этим число было: ", programnumber)
+		} else {
+			i = 0
+			fmt.Println("Хорош!!! Ты угадал!!!! ")
+
+		}
+		if i == 0 {
+			fmt.Println("Этим число было: ", programnumber)
+			fmt.Println("\nДавай сыграем ещё раз")
+			check()
+		} else {
+			fmt.Println("Осталось попыток: ", i, "Угадай снова какое число загдал")
+			fmt.Scanf("%d\n", &usernumber)
+		}
+
 	}
-	fmt.Println("\nДавай сыграем ещё раз")
-	check()
+
 }
 
 func generatornumber() int {
 	var generator int = rand.Intn(20)
 	return generator
-
 }
